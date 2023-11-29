c30fbbb2fec5c2ff8255d6a0fd6fdb9415189033
Ijjoe <cijjj9220@gmail.com>
2023-11-29 17:45:01 +0900

    폴더생성 초기작업

---
+++ 리액트 솔컴.mm
@@ -0,0 +1,118 @@
+<map version="freeplane 1.11.5">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="리액트 솔컴" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1090958577" CREATED="1409300609620" MODIFIED="1701160520814">
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<font BOLD="true"/>
+<hook NAME="MapStyle" background="#002b36" zoom="0.826">
+    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_note_icons="true" associatedTemplateLocation="template:/dark_solarized_template.mm" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_1664261437" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#002b36" BACKGROUND_COLOR="#fdf6e3" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#f0f0f0" BORDER_DASH_LIKE_EDGE="true" BORDER_DASH="SOLID" VGAP_QUANTITY="3 pt">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#fdf6e3" WIDTH="2" TRANSPARENCY="255" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_1664261437" STARTINCLINATION="132.75 pt;-31.5 pt;" ENDINCLINATION="132.75 pt;24 pt;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="11" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
+<edge STYLE="bezier" COLOR="#93a1a1" WIDTH="3" DASH="SOLID"/>
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details" COLOR="#fdf6e3" BACKGROUND_COLOR="#073642">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#2aa198" TEXT_ALIGN="LEFT">
+<icon BUILTIN="clock2"/>
+<font SIZE="10" ITALIC="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" COLOR="#fdf6e3" BACKGROUND_COLOR="#d33682" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#d33682"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_1940777954" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#bf616a">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#bf616a" TRANSPARENCY="255" DESTINATION="ID_1940777954"/>
+<font NAME="Ubuntu" SIZE="12" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.flower" COLOR="#ffffff" BACKGROUND_COLOR="#255aba" STYLE="oval" TEXT_ALIGN="CENTER" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="22 pt" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#f9d71c" BORDER_DASH_LIKE_EDGE="false" BORDER_DASH="CLOSE_DOTS" MAX_WIDTH="6 cm" MIN_WIDTH="3 cm"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#ffffff" BACKGROUND_COLOR="#002b36" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt" BORDER_WIDTH="3.1 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_DASH_LIKE_EDGE="true">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#fdf6e3" BACKGROUND_COLOR="#073642" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#fdf6e3" BACKGROUND_COLOR="#586e75" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR="#f0f0f0">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#fdf6e3" BACKGROUND_COLOR="#657b83" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR_LIKE_EDGE="true">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#fdf6e3" BACKGROUND_COLOR="#839496" BORDER_COLOR_LIKE_EDGE="true">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" COLOR="#fdf6e3" BACKGROUND_COLOR="#93a1a1" BORDER_COLOR_LIKE_EDGE="true">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" BACKGROUND_COLOR="#eee8d5" BORDER_COLOR_LIKE_EDGE="true">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" BACKGROUND_COLOR="#eedfcc" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" BACKGROUND_COLOR="#fdf6e3" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" BORDER_COLOR="#f0f0f0">
+<font SIZE="9"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node TEXT="윈도우설치" POSITION="top_or_left" ID="ID_1329581513" CREATED="1701160701890" MODIFIED="1701160705512">
+<node TEXT="npx create-react-app front-app --template typescript&#xa;&#xa;npm i --save react react-dom typescript&#xa;$npm i --save-dev @types/react @types/react-dom @types/node&#xa;&#xa; 프로젝트 루트 폴더에서 &quot;npx eslint --init&quot; 입력&#xa;$npx eslint --init" POSITION="top_or_left" ID="ID_179993082" CREATED="1701160565278" MODIFIED="1701160886467"/>
+<node TEXT="넥스트 js 리액트 설치시 명령어" ID="ID_945808020" CREATED="1701161303369" MODIFIED="1701161315408">
+<node TEXT="npx create-next-app@13.4.5 [프로젝트 이름]" ID="ID_151943360" CREATED="1701161316732" MODIFIED="1701161320058"/>
+<node TEXT="npx create-next-app@13.4.5 front-app --template typescript" ID="ID_193039664" CREATED="1701161521013" MODIFIED="1701161562098">
+<node TEXT="TDD설치" ID="ID_950642776" CREATED="1701162439155" MODIFIED="1701162461496">
+<node TEXT="npm install --save-dev jest jest-environment-jsdom @testing-library/react @testing-library/jest-dom" POSITION="top_or_left" ID="ID_801254051" CREATED="1701162415163" MODIFIED="1701162417003"/>
+</node>
+</node>
+<node TEXT="실행 에러" ID="ID_1646213702" CREATED="1701161813033" MODIFIED="1701161817223">
+<node TEXT="[Error: ENOENT: no such file or directory, open &apos;D:\front-app\.next\BUILD_ID&apos;] {&#xa;  errno: -4058,&#xa;  code: &apos;ENOENT&apos;,&#xa;  syscall: &apos;open&apos;,&#xa;  path: &apos;D:\\front-app\\.next\\BUILD_ID&apos;&#xa;}" ID="ID_661270692" CREATED="1701161818927" MODIFIED="1701161821987">
+<node TEXT="개발 실행 -&gt; npm run dev&#xa;빌드 하기 -&gt; npm run build&#xa;빌드 실행 -&gt; npm run start 를 확인 할 수 있다." ID="ID_341710951" CREATED="1701163461464" MODIFIED="1701163463442">
+<node TEXT="npm run dev 으로 실행" POSITION="top_or_left" ID="ID_1550519918" CREATED="1701161864241" MODIFIED="1701161870312"/>
+</node>
+</node>
+</node>
+<node TEXT="React 프로젝트에서 package.json 파일에 새로운 디펜던시(의존성)를 추가한 후에는 보통 npm install 명령어를 사용하여 해당 디펜던시를 설치합니다. npm update 명령어는 기존에 설치된 패키지들을 최신 버전으로 업데이트하는 데 사용되며, 새로운 디펜던시를 설치하는 용도로는 사용되지 않습니다." ID="ID_146288762" CREATED="1701163348800" MODIFIED="1701163350915"/>
+</node>
+<node TEXT="$ npm install -D cypress&#xa;$ npm install -D eslint-plugin-cypress // eslint를 사용하는 경우만 추가" ID="ID_1874171834" CREATED="1701232115224" MODIFIED="1701232117991">
+<node TEXT="{&#xa;  &quot;scripts&quot;: {&#xa;    &quot;cypress&quot;: &quot;npx cypress open&quot;&#xa;  },&#xa;}" ID="ID_1428088571" CREATED="1701232133861" MODIFIED="1701232136664"/>
+<node TEXT="eslint 사용시" ID="ID_157244577" CREATED="1701232143578" MODIFIED="1701232148557">
+<node TEXT="{&#xa;    &quot;extends&quot;: [&#xa;        &quot;plugin:cypress/recommended&quot;&#xa;    ],&#xa;}" ID="ID_1063384344" CREATED="1701232151296" MODIFIED="1701232155040"/>
+</node>
+</node>
+</node>
+<node TEXT="common" POSITION="bottom_or_right" ID="ID_1622034039" CREATED="1701242078365" MODIFIED="1701242084439">
+<node TEXT="components" POSITION="bottom_or_right" ID="ID_1911458350" CREATED="1701231676685" MODIFIED="1701231685404">
+<node TEXT="datePicker" ID="ID_91477282" CREATED="1701231687932" MODIFIED="1701231692835"/>
+<node TEXT="headermenu" ID="ID_1293762357" CREATED="1701231695387" MODIFIED="1701231700771"/>
+<node TEXT="headerMobileMenu" ID="ID_588480355" CREATED="1701231702244" MODIFIED="1701231710074"/>
+</node>
+<node TEXT="componentsFooter" ID="ID_148052607" CREATED="1701242094230" MODIFIED="1701242110037">
+<node TEXT="termsLocation_test" ID="ID_937478402" CREATED="1701242115700" MODIFIED="1701242133405"/>
+</node>
+</node>
+</node>
+</map>
